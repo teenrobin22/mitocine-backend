beb547d4efb651c92ecea3b454dc8f711e96f3bc
desarrollo11 <desarrollo11@pcdesa11.aate.gob.pe>
2019-08-08 16:50:38 -0500

    2do commit

--- cineapp-backend/src/main/java/com/mitocode/controller/UsuarioController.java
+++ cineapp-backend/src/main/java/com/mitocode/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,12 +33,12 @@ public class UsuarioController {
 	@Autowired
 	private BCryptPasswordEncoder bcrypt;
 	
-	/*@PostMapping(produces = "application/json", consumes = "application/json")
+	@PostMapping(produces = "application/json", consumes = "application/json")
 	private ResponseEntity<Object> registrar(@RequestBody Usuario usuario){		
 		usuario.setPassword(bcrypt.encode(usuario.getPassword()));
 		service.registrarTransaccional(usuario);
 		return new ResponseEntity<Object>(HttpStatus.CREATED);
-	}*/
+	}
 	
 	@PostMapping
 	private ResponseEntity<Object> registrar(@RequestPart("usuario")  Usuario usuario , @RequestPart("file") MultipartFile file)		
